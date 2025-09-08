46f298b25f6a9dec9bbf8a82471ebabc6b9cbfc7
krishnakr05 <krishnakr022021@gmail.com>
2025-09-08 22:13:52 +0530

    Set up grid

---
+++ myscript.js
@@ -0,0 +1,8 @@
+const container= document.getElementById('container');
+const frag= document.createDocumentFragment();
+for(let i=0; i< 256; i++){
+    const sq= document.createElement('div');
+    sq.className= 'square';
+    frag.appendChild(sq)
+}
+container.appendChild(frag);
