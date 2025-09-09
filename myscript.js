b45db1116cd253f357ec7e57ee756213c86b801e
krishnakr05 <krishnakr022021@gmail.com>
2025-09-09 22:48:14 +0530

    Added functionality for custom grid size

--- myscript.js
+++ myscript.js
@@ -1,14 +1,53 @@
 const container= document.getElementById('container');
+/*
 const frag= document.createDocumentFragment();
 for(let i=0; i< 256; i++){
     const sq= document.createElement('div');
     sq.className= 'square';
+    
+    sq.style.flex=`0 0 calc(100%/16)`;
+
+    sq.addEventListener('mouseenter', () => {
+        sq.style.backgroundColor = "black";
+    });
+
+
     frag.appendChild(sq)
 }
 container.appendChild(frag);
-const squares= document.querySelectorAll('.square');
-squares.forEach(square=>{
-    square.addEventListener('mouseenter', ()=>{
-        square.style.backgroundColor= "black";
-    })
+*/
+
+function createGrid(n){
+    const frag= document.createDocumentFragment();
+    for(let i=0; i< (n*n); i++){
+        const sq= document.createElement('div');
+        sq.className= 'square';
+        
+        sq.style.flex=`0 0 calc(100%/${n})`;
+
+        sq.addEventListener('mouseenter', () => {
+            sq.style.backgroundColor = "black";
+        });
+
+
+        frag.appendChild(sq)
+    }
+    container.appendChild(frag);
+
+}
+
+const btn= document.getElementById('btn');
+btn.addEventListener('click', ()=>{
+    let input= prompt("Enter grid size ")
+    let gridsize=parseInt(input);
+    if(isNaN(gridsize)|| gridsize<1 || gridsize>100){
+        prompt('Please enter a value between 1 and 100')
+    }
+    else{
+        container.innerHTML = ""
+        createGrid(gridsize)
+    }
 })
+
+createGrid(16);
+
